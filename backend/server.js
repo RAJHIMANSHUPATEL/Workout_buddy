9963a18d6f059efc5459432225cedbc52dd0e5e6
RAJHIMANSHUPATEL <blazehimanshuraj@gmail.com>
2024-09-06 15:07:12 +0530

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const wrokoutRoutes = require('./routes/workouts');
+const workoutRoutes = require('./routes/workouts');
 const userRoutes = require('./routes/user');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -19,7 +19,7 @@ app.use((req,res,next)=>{
 })
 
 // routes
-app.use('/api/workouts', wrokoutRoutes);
+app.use('/api/workouts', workoutRoutes);
 app.use('/api/user', userRoutes);
 
 app.get('/', (req,res)=> {
@@ -31,7 +31,7 @@ mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
     // listening for request 
     app.listen(process.env.PORT, ()=>{
-        console.log("DB connection successful & Listening on port 3000");
+        console.log(`DB connection successful & Listening on port ${process.env.PORT}`);
     })
 })
 .catch((error)=>{
