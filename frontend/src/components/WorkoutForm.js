a651f8aed8298df08b991bbd85bfe40429f1db53
RAJHIMANSHUPATEL <blazehimanshuraj@gmail.com>
2024-09-06 15:46:50 +0530

    backend-link

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -22,7 +22,7 @@ function WorkoutForm() {
 
         const workout = {title, load, reps}
 
-        const response = await fetch('/api/workouts', {
+        const response = await fetch('https://workout-buddy-1-en3z.onrender.com/api/workouts', {
             method: 'POST',
             body: JSON.stringify(workout),
             headers: {
