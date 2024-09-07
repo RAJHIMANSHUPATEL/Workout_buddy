d81400627b3b0ef41eb0201d43aed63c3dfe506b
RAJHIMANSHUPATEL <blazehimanshuraj@gmail.com>
2024-09-07 16:49:07 +0530

    added-link

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = ()=>{
 
     useEffect(()=>{
         const fetchWorkouts = async ()=>{
-            const response = await fetch('/api/workouts', {
+            const response = await fetch('https://workout-buddy-lq7f.onrender.com/api/workouts', {
                 headers: {
                     'Autherization': `Bearer ${user.token}`
                 }
